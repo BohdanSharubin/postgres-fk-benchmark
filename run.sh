d92f40d6b403c55c2541bea93435149292c89cdc
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-03-19 22:48:44 +0200

    Modify run script

--- run.sh
+++ run.sh
@@ -21,17 +21,50 @@ echo "===== FK TEST ====="
 psql -U postgres -f schema/schema_fk.sql
 psql -U postgres -f data/init.sql
 
+echo "Analyzing..."
+psql -U postgres -c "ANALYZE;"
+
 echo "Running INSERT benchmark (FK)"
 
-pgbench -U postgres -c 20 -j 4 -T 60  \
+pgbench -U postgres -c 20 -j 4 -T 180  \
 -f tests/insert_likes.sql postgres \
 > $RESULT_DIR/fk_insert.txt 2>&1
 
+echo "Analyzing..."
+psql -U postgres -c "ANALYZE;"
+
 echo "Running DELETE benchmark (FK)"
 
-pgbench -U postgres -c 1 -j 1 -T 60  \
+pgbench -U postgres -c 1 -j 1 -T 180  \
 -f tests/fk_delete.sql postgres \
 > $RESULT_DIR/fk_delete.txt 2>&1
+
+echo "Cleaning database"
+
+psql -U postgres -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+
+echo "===== FK WITHOUT INDEXES TEST ====="
+
+psql -U postgres -f schema/schema_fk_without_indexes.sql
+psql -U postgres -f data/init.sql
+
+echo "Analyzing..."
+psql -U postgres -c "ANALYZE;"
+
+echo "Running INSERT benchmark (FK WITHOUT INDEXES)"
+
+pgbench -U postgres -c 20 -j 4 -T 180  \
+-f tests/insert_likes.sql postgres \
+> $RESULT_DIR/fk_without_indexes_insert.txt 2>&1
+
+echo "Analyzing..."
+psql -U postgres -c "ANALYZE;"
+
+echo "Running DELETE benchmark (FK WITHOUT INDEXES)"
+
+pgbench -U postgres -c 1 -j 1 -T 180  \
+-f tests/fk_delete.sql postgres \
+> $RESULT_DIR/fk_without_indexes_delete.txt 2>&1
 
 echo "Cleaning database"
 
@@ -44,13 +77,19 @@ echo "===== WITHOUT FK TEST ====="
 psql -U postgres -f schema/schema_without_fk.sql
 psql -U postgres -f data/init.sql
 
+echo "Analyzing..."
+psql -U postgres -c "ANALYZE;"
+
 echo "Running INSERT benchmark (NO FK)"
-pgbench -U postgres -c 20 -j 4 -T 60 \
+pgbench -U postgres -c 20 -j 4 -T 180 \
 -f tests/insert_likes.sql postgres \
 > $RESULT_DIR/without_fk_insert.txt 2>&1
 
+echo "Analyzing..."
+psql -U postgres -c "ANALYZE;"
+
 echo "Running DELETE benchmark (NO FK)"
-pgbench -U postgres -c 1 -j 4 -T 60 \
+pgbench -U postgres -c 1 -j 4 -T 180 \
 -f tests/without_fk_delete.sql postgres \
 > $RESULT_DIR/without_fk_delete.txt 2>&1
 
